7548bb9f45d89845387dc16a9ca1bef99e32a3e7
Mohammad Sami <mohammadsami@duck.com>
2022-08-15 02:28:52 +0530

    add items page with data

--- src/components/pages/categoryMenu.jsx
+++ src/components/pages/categoryMenu.jsx
@@ -1,27 +1,36 @@
+//package
+import { useParams, Link } from "react-router-dom";
 
-const CategoryMenu = () => { 
-    return (
-        <div className="categoryMenu">
-        <div>
-            <ul>
-                <li className="li0">See Categories 👉 </li>
-                <li className="li1">SHARING PLATES-VEG</li>
-                <li className="li2">SHARING PLATES- NONVEG</li>
-                <li className="li3">STUFFED BAO/PAV</li>
-                <li className="li4">EGGS TO ORDER</li>
-                <li className="li5">MOMOS</li>
-                <li className="li6">SHAWARMA</li>
-                <li className="li7">WRAPS AND ROLLS</li>
-                <li className="li8">PIZZA</li>
-                <li className="li9">OPEN SANDWICHES</li>
-                <li className="li11">BURGERS</li>
-                <li className="li12">PASTA</li>
-                <li className="li10">DESSERTS</li>
-                <li className="li13">HOT BEVERAGES</li>
-                <li className="li14">COLD BEVERAGES</li>
-            </ul>
-        </div>
-        </div>
+//data
+import categoryData from "../../Data/categoryData.json";
+
+const CategoryMenu = () => {
+    const { slug } = useParams();
+    const category = categoryData.categories.find(
+        (category) => category.slug === slug
     );
-}
+
+	return (
+		<div className="categoryMenu">
+			<div>
+				<ul>
+					<li className="li0">See Dishes 👉 </li>
+                    
+                        {category.items.map((dish) => {
+                            return (
+                                <li key={dish._uid}>
+                                    <h1>
+                                        {dish.name}
+                                    </h1>
+                                    <img src={dish.image} alt="" />
+                                </li>
+                            );
+                        })}
+                    
+					<li className="li1">👈 See Categories</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
 export default CategoryMenu;
